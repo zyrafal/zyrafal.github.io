9a717c36dc0f620413782e0d1581d448a67a3d7f
rafal <rafal.szostak.123@gmail.com>
2021-03-26 15:28:54 +0100

    Idk how to name it 2

--- zyrafal.js
+++ zyrafal.js
@@ -1,10 +1,28 @@
-function init() {
-    var keys = "1234567890";
+function httpGet(theUrl) {
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+    {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            return xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET", theUrl, false );
+    xmlhttp.send();
 }
 
-function openWebsite() {
-    var urlInput = document.getElementById("input");
-    if (urlInput.checkValidity()) {
-        window.open(urlInput.value, "_self");
-    }
-};
+function testFunction() {
+//    var page = document.getElementById("page").contentWindow.document;
+//    console.log(page);
+
+    var page = httpGet("http://www.kurnik.pl");
+    document.write(page);
+
+}
